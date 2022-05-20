39abdd894816e1ceaf405fad8d66424acfebcaa8
KairatSS <kairat.seksembaev@gmail.com>
2022-05-20 23:49:26 +0800

    frontend layout and react routing established

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Cart from "./components/Cart";
+import { Container } from "react-bootstrap";
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Mega shop</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            {/* subject to change */}
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
